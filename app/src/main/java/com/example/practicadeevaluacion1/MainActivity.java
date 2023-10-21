c8ff72aa74858ad5d91bad321f1fefa7026940ef
IvanCuestaGarcia <icuegar696@g.educaand.es>
2023-10-22 01:29:32 +0200

    aplicacion finalizada

--- app/src/main/java/com/example/practicadeevaluacion1/MainActivity.java
+++ app/src/main/java/com/example/practicadeevaluacion1/MainActivity.java
@@ -14,23 +14,19 @@ public class MainActivity extends AppCompatActivity {
 
     public void layout1() {
         setContentView(R.layout.layout1);
+
+        final Button btnAcceder = findViewById(R.id.btnAcceso);
+
+        btnAcceder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout2();
+            }
+        });
     }
 
     public void layout2() {
         setContentView(R.layout.layout2);
-    }
-
-    public void layout3() {
-        setContentView(R.layout.layout3);
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        layout1();
-        //layout2();
-        //layout3();
 
         final EditText edtxtNom = findViewById(R.id.edtxtNombre);
         final EditText edtxtFecha = findViewById(R.id.edtxtFecha);
@@ -65,32 +61,74 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout3();
+            }
+        });
+    }
+
+    public void layout3() {
+        setContentView(R.layout.layout3);
+
         final RatingBar utilidad = findViewById(R.id.ratingUtilidad);
         final RatingBar diseno = findViewById(R.id.ratingDiseno);
         final RatingBar funcionalidad = findViewById(R.id.ratingFuncionalidad);
         final TextView txtMandado = findViewById(R.id.txtMandado);
+        final TextView txtUtilidad = findViewById(R.id.txtUtilidad);
+        final TextView txtDiseño = findViewById(R.id.txtDiseño);
+        final TextView txtFuncionalidad = findViewById(R.id.txtFuncionalidad);
+        final Button btnMandar = findViewById(R.id.btnMandar);
 
         utilidad.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-                txtMandado.append("Utilidad: " + rating + "\n");
+                txtUtilidad.setText("Utilidad: " + rating);
             }
         });
 
         diseno.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-                txtMandado.append("Diseño: " + rating + "\n");
+                txtDiseño.setText("Diseño: " + rating);
             }
         });
 
         funcionalidad.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-                txtMandado.append("Funcionalidad: " + rating + "\n");
+                txtFuncionalidad.setText("Funcionalidad: " + rating);
             }
         });
 
+        btnMandar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtMandado.setVisibility(View.VISIBLE);
+                txtUtilidad.setVisibility(View.VISIBLE);
+                txtDiseño.setVisibility(View.VISIBLE);
+                txtFuncionalidad.setVisibility(View.VISIBLE);
+            }
+        });
+
+        final Button btnInicio = findViewById(R.id.btnInicio);
+
+        btnInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout1();
+            }
+        });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        layout1();
+        //layout2();
+        //layout3();
 
     }
 
